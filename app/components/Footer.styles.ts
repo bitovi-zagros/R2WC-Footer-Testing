b3aaca8f496e6040dca2b52b297b987696864c1c
Zagros Maroofzadeh <zagros@Zagross-MacBook-Pro.local>
2023-05-18 17:07:30 -0400

    add components

---
+++ app/components/Footer.styles.ts
@@ -0,0 +1,10 @@
+import css from "styled-jsx/css";
+
+const styles = css`
+    Footer{
+        background-color:red;
+        height:500px;
+        width:500px;
+    }
+`;
+export default styles;
---
+++ app/components/Footer.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import styles from './Footer.styles';
+export const Footer = () => {
+  return <footer>
+    <style jsx>{styles}</style>
+    testing
+    </footer>;
+};
+export default Footer;
